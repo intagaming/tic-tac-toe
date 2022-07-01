c255a1ced605ba730044ea944b0535598110cbc9
An Hoang <xuanan2001@pm.me>
2022-07-01 17:56:18 +0700

    fix loading image condition

--- src/components/PleaseLogin.tsx
+++ src/components/PleaseLogin.tsx
@@ -6,7 +6,7 @@ const PleaseLogin = () => {
 
   return (
     <div className="fixed top-0 left-0 flex flex-col items-center justify-center w-screen h-screen text-white bg-black bg-opacity-70">
-      {status === "authenticated" && (
+      {status === "loading" && (
         <div className="relative w-20 h-20">
           <Image src="/rings.svg" layout="fill" />
         </div>
