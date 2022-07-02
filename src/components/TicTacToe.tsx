0a2a293fe6c305cedadbcd54d7847a3a126d3bb2
An Hoang <xuanan2001@pm.me>
2022-07-02 21:09:50 +0700

    join room

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useChannel from "../hooks/useChannel";
 import { trpc } from "../utils/trpc";
 import { useAbly } from "./AblyContext";
@@ -11,6 +11,7 @@ const TicTacToe = () => {
 
   const { setTokenRequest } = useAbly();
   const [roomId, setRoomId] = useState<string | undefined>();
+  const [clientId, setClientId] = useState<string | undefined>();
 
   const controlChannel = useChannel(`control:${roomId}`);
 
@@ -20,18 +21,22 @@ const TicTacToe = () => {
         setLoading(false);
         setTokenRequest(newRoom.data.tokenRequestData);
         setRoomId(newRoom.data.roomId);
+        setClientId(newRoom.data.clientId);
       }
       // TODO: handle error
     }
   }, [newRoom.data, newRoom.isError, newRoom.isLoading, setTokenRequest]);
+
+  const joinRoom = trpc.useMutation(["tictactoe.join-room"]);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   return (
     <div className="w-full h-full bg-green-700">
       {loading && <p>Loading...</p>}
       {newRoom.data && (
         <div>
-          <p>Client ID: {newRoom.data.clientId}</p>
-          <p>Room ID: {newRoom.data.roomId}</p>
+          <p>Client ID: {clientId}</p>
+          <p>Room ID: {roomId}</p>
           <button
             type="button"
             onClick={() => {
@@ -40,6 +45,27 @@ const TicTacToe = () => {
           >
             Send a message
           </button>
+          <div className="flex">
+            <input type="text" ref={inputRef} />
+            <button
+              type="button"
+              onClick={() => {
+                if (!inputRef.current) return;
+                joinRoom.mutate(
+                  { roomId: inputRef.current.value },
+                  {
+                    onSuccess: (data) => {
+                      setTokenRequest(data.tokenRequestData);
+                      setRoomId(data.roomId);
+                      setClientId(data.clientId);
+                    },
+                  }
+                );
+              }}
+            >
+              Join Room
+            </button>
+          </div>
         </div>
       )}
     </div>
--- src/server/router/tictactoe.ts
+++ src/server/router/tictactoe.ts
@@ -1,6 +1,7 @@
 import { TRPCError } from "@trpc/server";
 import * as Ably from "ably/promises";
 import crypto from "node:crypto";
+import { z } from "zod";
 import { createRouter } from "./context";
 
 const MAX_RETRIES = 5;
@@ -29,64 +30,114 @@ const DEFAULT_ROOM: Room = {
   },
 };
 
-export default createRouter().query("new-room", {
-  async resolve({ ctx }) {
-    const apiKey = process.env.ABLY_API_KEY;
-    if (!apiKey) {
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Error 1",
-      });
-    }
+export default createRouter()
+  .query("new-room", {
+    async resolve({ ctx }) {
+      const apiKey = process.env.ABLY_API_KEY;
+      if (!apiKey) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Error 1",
+        });
+      }
 
-    const { session, redis } = ctx;
+      const { session, redis } = ctx;
 
-    let clientId;
-    if (!session?.user) {
-      clientId = crypto.randomUUID();
-    } else {
-      clientId = `${session.user.name}_${session.user.email}`;
-    }
+      let clientId;
+      if (!session?.user) {
+        clientId = crypto.randomUUID();
+      } else {
+        clientId = `${session.user.name}_${session.user.email}`;
+      }
 
-    // Create a new room
-    let retries = 0;
-    let roomId;
-    while (retries <= MAX_RETRIES) {
-      roomId = crypto.randomBytes(3).toString("hex");
-      try {
+      // Create a new room
+      let retries = 0;
+      let roomId;
+      while (retries <= MAX_RETRIES) {
+        roomId = crypto.randomBytes(3).toString("hex");
         // eslint-disable-next-line no-await-in-loop
         const json = await redis.call(
           "JSON.SET",
           `room:${roomId}`,
           "$",
-          JSON.stringify({ ...DEFAULT_ROOM, host: clientId }),
+          JSON.stringify({ ...DEFAULT_ROOM }),
           "NX"
         );
-        if (json !== null) break;
-      } catch (e) {
+        if (json !== null) {
+          // eslint-disable-next-line no-await-in-loop
+          redis.expire(`room:${roomId}`, 60); // The host has 60 seconds to join the room
+          break;
+        }
+
         retries += 1;
       }
-    }
-    if (retries > MAX_RETRIES || roomId === undefined) {
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Unable to create a room id. Try again later.",
+      if (retries > MAX_RETRIES || roomId === undefined) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Unable to create a room id. Try again later.",
+        });
+      }
+
+      // Generate the Ably API key to communicate within the room
+      const client = new Ably.Realtime(apiKey);
+      const tokenRequestData = await client.auth.createTokenRequest({
+        clientId,
+        capability: {
+          [`control:${roomId}`]: ["publish"],
+        },
       });
-    }
 
-    // Generate the Ably API key to communicate within the room
-    const client = new Ably.Realtime(apiKey);
-    const tokenRequestData = await client.auth.createTokenRequest({
-      clientId,
-      capability: {
-        [`control:${roomId}`]: ["publish"],
-      },
-    });
+      return {
+        clientId,
+        roomId,
+        tokenRequestData,
+      };
+    },
+  })
+  .mutation("join-room", {
+    input: z.object({
+      roomId: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      const apiKey = process.env.ABLY_API_KEY;
+      if (!apiKey) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Error 1",
+        });
+      }
 
-    return {
-      clientId,
-      roomId,
-      tokenRequestData,
-    };
-  },
-});
+      const { roomId } = input;
+
+      const { session, redis } = ctx;
+
+      let clientId;
+      if (!session?.user) {
+        clientId = crypto.randomUUID();
+      } else {
+        clientId = `${session.user.name}_${session.user.email}`;
+      }
+
+      const json = await redis.call("JSON.GET", `room:${roomId}`, "$");
+      if (json === null) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Room not found",
+        });
+      }
+
+      const client = new Ably.Realtime(apiKey);
+      const tokenRequestData = await client.auth.createTokenRequest({
+        clientId,
+        capability: {
+          [`control:${roomId}`]: ["publish"],
+        },
+      });
+
+      return {
+        clientId,
+        roomId,
+        tokenRequestData,
+      };
+    },
+  });
