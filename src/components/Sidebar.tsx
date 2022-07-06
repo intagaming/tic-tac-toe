4a5c746f96aba861cbea3770e14d666e46c5b429
An Xuan Hoang <xuanan2001@gmail.com>
2022-07-06 15:59:28 +0700

    login button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { signOut, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Sidebar = () => {
   const { data: session, status } = useSession();
@@ -6,6 +6,11 @@ const Sidebar = () => {
   return (
     <div className="w-60">
       <h1>Hi{session?.user && `, ${session.user.name}`}</h1>
+      {status === "unauthenticated" && (
+        <button type="button" onClick={() => signIn()}>
+          Sign In
+        </button>
+      )}
       {status === "authenticated" && (
         <button type="button" onClick={() => signOut()}>
           Sign Out
